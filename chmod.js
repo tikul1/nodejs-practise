3dd92d3c7b534c4604d3f5a74142ad6fce94c756
Hardik Parmar <hardik.parmar5@gmail.com>
2022-03-22 11:49:02 +0530

    updating chmod

--- chmod.js
+++ chmod.js
@@ -2,13 +2,23 @@ const fs = require("fs");
 
 console.log("granting read access only");
 fs.chmod("hardik2.txt", 0o400, () => {
-    console.log("\nreading file contents");
-    console.log(fs.readFileSync("hardik2.txt", 'utf8'));
-    console.log("\ntrying to write to file");
-    try { 
-        fs.writeFileSync('hardik2.txt', 'edit this file');
-    }
-    catch (e) {
-        console.log("error code", e.code);
-    }
+  console.log("\nreading file contents");
+  console.log(fs.readFileSync("hardik2.txt", "utf8"));
+  console.log("\ntrying to write to file");
+  try {
+    fs.writeFileSync("hardik2.txt", "edit this file");
+  } catch (e) {
+    console.log("error code", e.code);
+  }
+  
+  //granting both permissions read and write
+  
+  console.log("\nGranting read and write access to user");
+  fs.chmod("hardik2.txt", 0o600, () => {
+      console.log("trying to write to file");
+      
+      fs.writeFileSync("hardik2.txt", "file has been edited");
+      console.log("\nreading the file components");
+      console.log(fs.readFileSync("hardik2.txt", "utf8"));
+    });
 });
